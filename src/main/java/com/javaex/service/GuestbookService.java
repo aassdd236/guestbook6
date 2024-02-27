a6785ef649d3ec0f4b2dc9b59cfb17f87365e215
aassdd236 <aassdd236@naver.com>
2024-02-27 17:59:18 +0900

    02/27

---
+++ src/main/java/com/javaex/service/GuestbookService.java
@@ -0,0 +1,52 @@
+package com.javaex.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javaex.dao.GuestbookDao;
+import com.javaex.vo.GuestVo;
+
+@Service
+public class GuestbookService {
+
+	@Autowired
+	private GuestbookDao guestbookDao;
+	
+	public List<GuestVo> exeList() {
+		System.out.println("GuestbookController.exeList()");
+		
+		List<GuestVo> guestbookList=guestbookDao.guestRead();
+		
+		return guestbookList;
+	}
+
+	public int exeWrite(GuestVo guestVo) {
+		System.out.println("GuestbookController.exeWrite()");
+		
+		int count = guestbookDao.guestWrite(guestVo);
+		
+		return count;
+		
+	}
+
+	public GuestVo exeDeleteForm(int no) {
+		System.out.println("GuestbookController.exeDeleteForm()");
+
+		GuestVo guestVo = guestbookDao.select(no);
+		
+		return guestVo;
+	}
+
+	public void exeDelete(int no, String password) {
+		System.out.println("GuestbookController.exeDelete()");
+		System.out.println(no);
+		System.out.println(password);
+		int count = guestbookDao.delete(no, password);
+	}
+
+	
+	
+
+}
